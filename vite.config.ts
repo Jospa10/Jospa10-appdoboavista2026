8731d75947acfe8c6e11dddbac96f16dc039a170
Jospa10 <josival422@gmail.com>
2026-02-14 10:11:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/', // ADICIONE ESTA LINHA AQUI
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '': path.resolve(__dirname, './src'),
-      },
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  // Isso força o Vite a olhar para a raiz, onde seu index.html está
+  root: './',
+  build: {
+    outDir: 'dist',
+  }
+})
+
 
