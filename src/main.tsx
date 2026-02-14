3bb486984ad58f36ef419a2a08ffdf4eb229d08a
Jospa10 <josival422@gmail.com>
2026-02-14 16:45:49 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App' // Certifique-se que está SEM o .tsx aqui
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
