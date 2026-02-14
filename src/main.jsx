209c0815aa15db3034bd35cfc6973fa4a1596212
Jospa10 <josival422@gmail.com>
2026-02-14 12:28:37 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Não foi possível encontrar o elemento raiz para montar o app.");
