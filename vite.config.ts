35e6f2fd26b2ade355c52df6c791043f8f605f0b
Jospa10 <josival422@gmail.com>
2026-02-14 10:14:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Isso força o Vite a olhar para a raiz, onde seu index.html está
-  root: './',
+  // Isso diz ao Vite: "O index.html está aqui na pasta principal, não procure na pasta src"
+  root: './', 
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
-
-
